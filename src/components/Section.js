2c7879a1e196a84ae945a1ee2b18bd4197c588c4
Leo Choi <leonardohchoi@gmail.com>
2023-09-23 19:53:40 -0300

    add styles to button container and buttons

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,7 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 function Section() {
-  return <Wrap>section</Wrap>;
+  return (
+    <Wrap>
+      <ItemText>
+        <h1>Model S</h1>
+        <h4>From $71,090*</h4>
+        <p>After Est. Gas Savings</p>
+      </ItemText>
+      <ButtonGroup>
+        <LeftButton>Order Now</LeftButton>
+        <RightButton>Demo Drive</RightButton>
+      </ButtonGroup>
+    </Wrap>
+  );
 }
 
 export default Section;
@@ -10,5 +22,35 @@ export default Section;
 const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  background: orange;
+  background-size: cover;
+  backgroun-position: center;
+  backgroun-repeat: no-repeat;
+  background-image: url("images/model-s.jpg");
+  display: flex;
+  flex-direction: column; // this changes the direction that justiy-content and align-items work
+  justify-content: space-between; // usually this aligns things horizontally but since we change the direction of flex-direction to colum now it aligns things vertically
+  align-items: center; // this is usually to align things vertically but now it will alignt things horizontally
 `;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin: 15px;
+`;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+  opacitu: 0.85;
+`;
+
+const RightButton = styled(LeftButton)``;
